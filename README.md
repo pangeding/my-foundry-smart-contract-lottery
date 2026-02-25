b5f3aaa0f5b676f1e310e079b7e40d5752db4515
pangeding <15533858+pangeding@user.noreply.gitee.com>
2026-02-25 23:57:39 +0800

    feat: the first part of the raffle contract

---
+++ README.md
@@ -0,0 +1,77 @@
+# Provably random raffle contract
+
+1. user can enter by paying a fee
+    1. the ticket fee will go to the winner after the draw
+2. after X peroid of time, the contract will pick a random winner
+    1. the winner will be picked randomly
+3. use chainlink VRF and Chainlink automation
+    1. Chainlink VRF for randomness
+    2. Chainlink automation for triggering the draw
+
+
+## Foundry
+
+**Foundry is a blazing fast, portable and modular toolkit for Ethereum application development written in Rust.**
+
+Foundry consists of:
+
+- **Forge**: Ethereum testing framework (like Truffle, Hardhat and DappTools).
+- **Cast**: Swiss army knife for interacting with EVM smart contracts, sending transactions and getting chain data.
+- **Anvil**: Local Ethereum node, akin to Ganache, Hardhat Network.
+- **Chisel**: Fast, utilitarian, and verbose solidity REPL.
+
+## Documentation
+
+https://book.getfoundry.sh/
+
+## Usage
+
+### Build
+
+```shell
+$ forge build
+```
+
+### Test
+
+```shell
+$ forge test
+```
+
+### Format
+
+```shell
+$ forge fmt
+```
+
+### Gas Snapshots
+
+```shell
+$ forge snapshot
+```
+
+### Anvil
+
+```shell
+$ anvil
+```
+
+### Deploy
+
+```shell
+$ forge script script/Counter.s.sol:CounterScript --rpc-url <your_rpc_url> --private-key <your_private_key>
+```
+
+### Cast
+
+```shell
+$ cast <subcommand>
+```
+
+### Help
+
+```shell
+$ forge --help
+$ anvil --help
+$ cast --help
+```
---
+++ src/Raffle.sol
@@ -0,0 +1,67 @@
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+
+/**
+ * @title A sample raffle contract
+ * @author pangeding
+ * @notice this contract is used for creating a sample raffle
+ * @dev this implement ChainlinkVRFV2
+ */
+contract Raffle{
+    error Raffle_NotEnoughEthSent();
+    
+    uint256 private immutable i_entranceFee = 0.01 ether;
+    address payable[] private s_players;
+
+    /** event */
+    event EnteredRaffle(address indexed player);
+
+    constructor(uint256 entranceFee){
+        i_entranceFee = entranceFee;
+    }
+
+    function enterRaffle() public payable{
+        if(msg.value < i_entranceFee){
+            revert Raffle_NotEnoughEthSent();
+        }   
+        s_players.push(payable(msg.sender));
+        // usage of event
+        // 1. make migration easier
+        // 2. make front end easier to "indexing"
+        emit EnteredRaffle(msg.sender);
+    }
+
+    // 1. pick a random number
+    // 2. use the random number to pick a player
+    // 3. function should be automatically called 
+    function pickWinner() public{
+        
+    }
+
+    /** getter functions */
+    function getEntranceFee() external view returns(uint256){
+        return i_entranceFee;
+    }
+}
